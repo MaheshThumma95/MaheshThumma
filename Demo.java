0c6951f61a36dc9f66e034002543ba28fc78c386
mahesh <mahesh@veniso.com>
2022-10-13 17:13:47 +0530

    added new line

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
     public static void main(String[] args) {
-        System.out.println("Hello Java Program");		
+        System.out.println("Hello Java Program");
+
+		System.out.println("Welcome"); //added
     }
 }
